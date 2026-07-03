20d944de81bc5a5e4c21f7667a618fa7528eb4b9
GymNest <krupeshparmar231@gmail.com>
2026-07-03 10:18:05 +0530

    Add shop info config

---
+++ src/config/shopInfo.js
@@ -0,0 +1,7 @@
+export const shopInfo = {
+  name: "Kashtbhanjan Medical",
+  address: "Plot No 84, Maruti Nandan, Keriya Road, Lathi, Amreli",
+  contact: "9558248839",
+  gstin: "",
+  email: ""
+};
